ee9a1be58dceffdc71f3344eb9813d1347235e46
DaniloAlmeidaSantos <danilo8almeida@hotmail.com>
2025-06-07 12:46:04 -0300

    Initial commit

---
+++ src/main/java/br/com/itau/banking_cancel_debit/BankingCancelDebitApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.banking_cancel_debit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingCancelDebitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingCancelDebitApplication.class, args);
+	}
+
+}
